1349ba3fca30fea16f70c126c88ef1e4309ca6d7
Serjo Bykov <serjobikov@gmail.com>
2024-07-31 11:04:13 +0300

    Create-ImageSearchApp

---
+++ ImageSearch/ImageSearch/ImageSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSearchApp.swift
+//  ImageSearch
+//
+//  Created by Sergio Mascarpone on 31.07.24.
+//
+
+import SwiftUI
+
+@main
+struct ImageSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
